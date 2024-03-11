d054665a1b293972caf85418e4d6e352d113dd3d
puppysh43 <131197935+puppysh43@users.noreply.github.com>
2024-03-11 18:25:19 -0400

    Update quests.rs

--- src/quests.rs
+++ src/quests.rs
@@ -1,9 +1,29 @@
 use crate::prelude::*;
 //when making a new quest it'll always be false
-pub type QuestDatabase = HashMap<String, Quest>;
+#[derive(Clone, Debug)]
+pub struct QuestDatabase {
+    contents: HashMap<String, Quest>,
+}
+impl QuestDatabase {
+    pub fn new(contents: HashMap<String, Quest>) -> Self {
+        Self { contents }
+    }
+    // pub fn new quest(key: String) -> &Quest {
+
+    // }
+}
 #[derive(Clone, Debug)]
 pub struct Quest {
     is_complete: bool,
-    mutually_exclusive: bool,
+    // mutually_exclusive: bool,
     choices: HashMap<String, Quest>,
+}
+
+impl Quest {
+    pub fn new(choices: HashMap<String, Quest>) -> Self {
+        Self {
+            is_complete: false,
+            choices,
+        }
+    }
 }
